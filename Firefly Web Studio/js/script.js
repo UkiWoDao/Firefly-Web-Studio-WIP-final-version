ef9d28f56b866ee4d4ea81da1a66068afa98e7ad
Uros Vucenovic <u_vucenovic@yahoo.com>
2019-03-04 11:26:04 +0100

    add different scroll height color change triggers

--- Firefly Web Studio/js/script.js
+++ Firefly Web Studio/js/script.js
@@ -1,39 +1,29 @@
-// sidebar menja boju po sekcijama
-// dodati active klasu za nav active
-// contact i soc icons menjaju boju po sekcijama
-
-// // make hamburger change color on scroll
-// // if (scroll  > innerHeight) {
-//     hamburgerDOM.classList.remove('defaultColor');
-//     hamburgerDOM.classList.add('secondaryColor');
-// }
-
-var hamburgerDOM = document.querySelectorAll('.hamburger div');
-var navDOM = document.querySelectorAll('.sidebar li');
-var socDOM = document.querySelectorAll('.icons i');
-var contDOM = document.getElementById('rotated-contact');
-var winH = window.innerHeight / 2;
-
 function changeHamburger() {
-  if (document.body.scrollTop || document.documentElement.scrollTop > winH) {
+  var hamburgerDOM = document.querySelectorAll('.hamburger div');
+  var fullHeight = window.innerHeight;
+  if (document.body.scrollTop || document.documentElement.scrollTop > fullHeight) {
       for (var i = 0; i < hamburgerDOM.length; i++) {
           hamburgerDOM[i].style.backgroundColor = 'var(--deep-sea)';
       }
   }
-  if (document.body.scrollTop || document.documentElement.scrollTop < winH) {
+  if (document.body.scrollTop || document.documentElement.scrollTop < fullHeight) {
       for (var i = 0; i < hamburgerDOM.length; i++) {
           hamburgerDOM[i].style.backgroundColor = 'var(--turquoise)';
       }
   }
 };
 
+// try make list items change sequentially on scroll
+
 function changeNav() {
-  if (document.body.scrollTop || document.documentElement.scrollTop > winH) {
+  var navDOM = document.querySelectorAll('.sidebar li');
+  var halfHeight = window.innerHeight / 2;
+  if (document.body.scrollTop || document.documentElement.scrollTop > halfHeight) {
       for (var i = 0; i < navDOM.length; i++) {
           navDOM[i].style.backgroundColor = 'var(--deep-sea)';
       }
   }
-  if (document.body.scrollTop || document.documentElement.scrollTop < winH) {
+  if (document.body.scrollTop || document.documentElement.scrollTop < halfHeight) {
       for (var i = 0; i < navDOM.length; i++) {
           navDOM[i].style.backgroundColor = 'var(--turquoise)';
       }
@@ -41,12 +31,14 @@ function changeNav() {
 };
 
 function changeSoc() {
-  if (document.body.scrollTop || document.documentElement.scrollTop > winH) {
+  var socDOM = document.querySelectorAll('.icons i');
+  var smallHeight = window.innerHeight / 10;
+  if (document.body.scrollTop || document.documentElement.scrollTop > smallHeight) {
       for (var i = 0; i < socDOM.length; i++) {
           socDOM[i].style.color = 'black';
       }
   }
-  if (document.body.scrollTop || document.documentElement.scrollTop < winH) {
+  if (document.body.scrollTop || document.documentElement.scrollTop < smallHeight) {
       for (var i = 0; i < socDOM.length; i++) {
           socDOM[i].style.color = 'var(--turquoise)';
       }
@@ -54,10 +46,12 @@ function changeSoc() {
 };
 
 function changeContact() {
-  if (document.body.scrollTop || document.documentElement.scrollTop > winH) {
+  var contDOM = document.getElementById('rotated-contact');
+  var halfHeight = window.innerHeight / 2;
+  if (document.body.scrollTop || document.documentElement.scrollTop > halfHeight) {
          contDOM.style.color = 'black';
   }
-  if (document.body.scrollTop || document.documentElement.scrollTop < winH) {
+  if (document.body.scrollTop || document.documentElement.scrollTop < halfHeight) {
         contDOM.style.color = 'var(--turquoise)';
   }
 };
